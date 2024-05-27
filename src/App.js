f97c03ac1f66b6fe1bf9bea39483ce654c2344a0
parkdayeong <45747299+parkdayeong@users.noreply.github.com>
2024-05-27 13:24:30 +0900

    useMemo branch -01

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React, { useRef, useEffect } from 'react';
-import Input from './components/Input';
+import React, { useMemo, useState } from 'react';
+
+const hardCalculate = (number) => {
+  console.log('hard calculate');
+  for (let i = 0; i < 9999999; i++) {} //생각하는시간
+  return number + 10000;
+};
+
+const easyCalculate = (number) => {
+  console.log('easy calculate');
+  return number + 1;
+};
 
 function App() {
-  const inputRef = useRef();
+  const [hardNumber, setHardNumber] = useState(1);
+  const [easyNumber, setEasyNumber] = useState(1);
 
-  const focus = () => {
-    inputRef.current.focus();
-  };
+  // const hardSum = hardCalculate(hardNumber);
+  const hardSum = useMemo(() => {
+    return hardCalculate(hardNumber);
+  }, [hardNumber]);
+  const easySum = easyCalculate(easyNumber);
 
   return (
     <>
-      <Input ref={inputRef} />
-      <button onClick={focus}>집중</button>
+      <h3>어려운계산기</h3>
+      <input
+        type='number'
+        value={hardNumber}
+        onChange={(e) => setHardNumber(parseInt(e.target.value))}
+      />
+      <span> + 10000 = {hardSum}</span>
+      <h3>쉬운계산기</h3>
+      <input
+        type='number'
+        value={easyNumber}
+        onChange={(e) => setEasyNumber(parseInt(e.target.value))}
+      />
+      <span> + 1 = {easySum}</span>
     </>
   );
 }
