ed2fbdc6a4cbec13f7f968af5473b45172df3258
parkdayeong <45747299+parkdayeong@users.noreply.github.com>
2024-05-27 17:09:34 +0900

    240527

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useReducer, useState } from 'react';
+
+// reducer - state를 업데이트하는 역할 (은행)
+// dispatch - state업데이트를 위한 요구
+// action - 요구의 내용
+
+const ACTION_TYPES = {
+  deposit: 'deposit',
+  withdraw: 'withdraw',
+};
+
+const reducer = (state, action) => {
+  console.log('reducer가 일을합니다.', state, action);
+  // //..
+  // return state + action.payload;
+  switch (action.type) {
+    case ACTION_TYPES.deposit:
+      return state + action.payload;
+    case ACTION_TYPES.withdraw:
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
 
 function App() {
   const [number, setNumber] = useState(0);
-  const [toggle, setToggle] = useState(true);
-
-  const someFunction = useCallback(() => {
-    console.log(`someFunc : number : ${number}`);
-    return;
-  }, [number]);
-
-  useEffect(() => {
-    console.log('someFunction이 변경되었습니다.');
-  }, [someFunction]);
+  const [money, dispatch] = useReducer(reducer, 0);
 
   return (
-    <>
-      <input type='number' value={number} onChange={(e) => setNumber(e.target.value)} />
-      <button onClick={() => setToggle(!toggle)}>{toggle.toString()}</button>
-      <br />
-      <button onClick={someFunction}>Call someFunc</button>
-    </>
+    <div>
+      <h2>useReducer 은행</h2>
+      <p>잔고: {money} 원</p>
+      <input
+        type='number'
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+        step='1000'
+      />
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPES.deposit, payload: number });
+        }}
+      >
+        예금
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPES.withdraw, payload: number });
+        }}
+      >
+        출금
+      </button>
+    </div>
   );
 }
 
