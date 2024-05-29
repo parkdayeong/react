cadd137c2c14c856a57f6bdfef958755e4d55803
parkdayeong <45747299+parkdayeong@users.noreply.github.com>
2024-05-29 11:06:24 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,87 +1,41 @@
-import React, { useReducer, useState } from 'react';
-import Students from './components/Students';
-
-const ACTION_TYPES = {
-  addStudent: 'add-student',
-  deleteStudent: 'delete-student',
-  markStudent: 'mark-student',
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ACTION_TYPES.addStudent:
-      console.log('add');
-      const name = action.payload.name;
-      const newStudent = {
-        id: Date.now(),
-        name,
-        isHere: false,
-      };
-      return {
-        count: state.count + 1,
-        students: [...state.students, newStudent],
-      };
-    case ACTION_TYPES.deleteStudent:
-      console.log('delete');
-      return {
-        count: state.count - 1,
-        students: state.students.filter((student) => student.id !== action.payload.id),
-      };
-    case ACTION_TYPES.markStudent:
-      return {
-        count: state.count,
-        students: state.students.map((student) => {
-          if (student.id === action.payload.id) {
-            return { ...student, isHere: !student.isHere };
-          }
-          return student;
-        }),
-      };
-    default:
-      return state;
-  }
-};
-
-const initialState = {
-  count: 0,
-  students: [],
-};
+import React, { useState, useReducer } from 'react';
+import { indexInitialState, ACTION_TYPES, reducer } from './reducer/indexReducer';
 
 const App = () => {
-  const [name, setName] = useState('');
-
-  const [studentInfo, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, indexInitialState);
 
   return (
     <div>
-      <h1>출석부</h1>
-      <p>총 학생 수 : {studentInfo.count}</p>
-      <input
-        type='text'
-        placeholder='이름을 입력해주세요'
-        value={name}
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      />
       <button
         onClick={() => {
-          dispatch({ type: ACTION_TYPES.addStudent, payload: { name } });
+          dispatch({ type: ACTION_TYPES.CHANGE_TO_KOR });
         }}
       >
-        추가
+        한글
       </button>
-      {studentInfo.students.map((student) => {
-        return (
-          <Students
-            key={student.id}
-            name={student.name}
-            dispatch={dispatch}
-            id={student.id}
-            isHere={student.isHere}
-          />
-        );
-      })}
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPES.CHANGE_TO_ENG });
+        }}
+      >
+        English
+      </button>
+      <div>{state.text}</div>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPES.PLUS_COUNT });
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTION_TYPES.MINUS_COUNT });
+        }}
+      >
+        -
+      </button>
+      <div>{state.count}</div>
     </div>
   );
 };
