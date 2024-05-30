48ba7797a6baee03ce5da37ff0db95b9b5f20622
parkdayeong <45747299+parkdayeong@users.noreply.github.com>
2024-05-30 11:41:08 +0900

    240530 customHook useFetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react';
+import { useFetch } from './useFetch';
+const baseUrl = 'https://jsonplaceholder.typicode.com';
 
 const App = () => {
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users');
-  }, []);
-
+  const { data: userData } = useFetch(baseUrl, 'users');
+  const { data: postData } = useFetch(baseUrl, 'posts');
+  const { data: todoData } = useFetch(baseUrl, 'todos');
   return (
     <div>
-      <h1>useFetch</h1>
+      <h1>user</h1>
+      {userData && <pre>{JSON.stringify(userData[0], null, 2)}</pre>}
+      <h1>post</h1>
+      {postData && <pre>{JSON.stringify(postData[0], null, 2)}</pre>}
+      <h1>todo</h1>
+      {todoData && <pre>{JSON.stringify(todoData[0], null, 2)}</pre>}
     </div>
   );
 };
