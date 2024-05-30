d3ac3739dd88e5d767075924eac5b9594d7c093f
parkdayeong <45747299+parkdayeong@users.noreply.github.com>
2024-05-30 09:54:15 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import React, { useCallback, useState } from 'react';
-import Child from './components/Child';
+import React from 'react';
 
 const App = () => {
-  const [parentAge, setParentAge] = useState(0);
-
-  const incrementParentAge = () => {
-    setParentAge(parentAge + 1);
-  };
-
-  console.log('👨‍👩‍👦부모 컴포넌트가 렌더링이 되었어요');
-
-  const tellMe = useCallback(() => {
-    console.log('😻콩이 사랑해');
-  }, []);
-
-  return (
-    <div style={{ border: '2px solid navy', padding: '10px' }}>
-      <h1>👨‍👩‍👦 부모</h1>
-      <p>age: {parentAge}</p>
-      <button onClick={incrementParentAge}>부모나이증가</button>
-      <Child name={'콩이'} tellMe={tellMe} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
